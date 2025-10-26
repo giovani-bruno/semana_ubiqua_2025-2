8b7c3911b6ef72179e6f89706af34817a35f92b3
Giovani Bruno <giovainic@gmail.com>
2025-10-26 20:16:20 -0300

    modified:   .gitignore

--- main.py
+++ main.py
@@ -4,26 +4,15 @@ from os import listdir
 def main():
     paginas = [
         st.Page("Sobre.py", title="👨‍💻 Sobre"),
-        # st.Page("Certificados.py", title="📃 Certificados"), 
-        # st.Page("Leituras.py", title="📚 Leituras"),
-        # st.Page("Projetos.py", title="💻 Projetos"),
         st.Page("Davi.py", title="🤖 Davi")
         ]
 
-    # for pagina in listdir("projetos"):
-    #     if pagina == "videos-imagens":
-    #         continue
-    #     paginas.append(st.Page(f"projetos/{pagina}"))
-    
     pg = st.navigation(paginas, position='hidden')
     
     pg.run()
 
 def barra_navegacao():
     st.sidebar.page_link("Sobre.py")
-    # st.sidebar.page_link("Certificados.py")
-    # st.sidebar.page_link("Leituras.py")
-    # st.sidebar.page_link("Projetos.py")
     st.sidebar.page_link("Davi.py")
 
 if __name__ == "__main__":
